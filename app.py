6f47407ce59ec739d8656133111d182972a320a3
Vineet Likhar <vineetlikhar1998@gmail.com>
2025-02-02 00:45:03 -0700

    fix: images to static

--- app.py
+++ app.py
@@ -9,6 +9,9 @@ nltk.download('punkt_tab')
 from nltk.tokenize import word_tokenize
 import matplotlib.pyplot as plt 
 import matplotlib.cm as cm
+import matplotlib
+matplotlib.use('agg')
+import seaborn as sns
 import os
 from datetime import datetime
 from sentence_transformers import SentenceTransformer
@@ -44,9 +47,11 @@ def predict():
     # data = [request.form.values()]
     label = ['Politics', 'Sports', 'Technology', 'Entertainment', 'Business']
     data = request.form['data']
-    print("data \n",data)
+    print("data preditct\n",data)
     doc2vec_data = doc2vec_model.infer_vector(word_tokenize(data))
-    sentenceTransformer_data = sentenceTransformer.encode([data])
+    print("doc2vec data", doc2vec_data)
+    model = SentenceTransformer('all-MiniLM-L6-v2')
+    sentenceTransformer_data = model.encode([data])
     pca_data = pca_model.transform(sentenceTransformer_data)
     print("pca data \n", pca_data)
     kmean_cluster = kmmeans_model.predict(pca_data)
@@ -54,31 +59,91 @@ def predict():
     doc2vec_data = np.array(doc2vec_data).reshape(1, -1)
     output = logregmodel.predict(doc2vec_data)[0]
     output = label[output]
+    print("output", output)
 
     # Get the matplotlib plot  
-    plot = get_plot(kmmeans_model)
+    # plot = get_plot(kmmeans_model)
+    plot = get_plot(kmmeans_model, pca_data)
 
     
   
     # Save the figure in the static directory  
     timestamp = str(datetime.now().timestamp())
+    print("timestamp", timestamp)
     plot.savefig(os.path.join('static', 'images', timestamp +'.png'))
     return render_template('home.html', prediction_text = '{}'.format(output), img_path = url_for('static', filename = 'images/' + timestamp + '.png'))
 
 # ################## For testing purpose ##################
-def get_plot(kmeans): 
+# def get_plot(kmeans): 
+#     df = pd.read_csv('pca.csv')
+#     X = df.iloc[:, 0].values
+#     Y = df.iloc[:, 1].values
+#     plt.scatter(X,Y,c = kmeans.labels_, cmap = 'viridis')
+#     plt.grid(True)
+#     for center in kmeans.cluster_centers_:
+#         center = center[:2]
+#         plt.scatter(center[0],center[1],marker = '^',c = 'red')
+#     plt.scatter(6.5, 4, marker='*')
+#     plt.xlabel("petal length (cm)")
+#     plt.ylabel("petal width (cm)")
+#     return plt
+
+def get_plot(kmeans, new_pca_point):
     df = pd.read_csv('pca.csv')
-    X = df.iloc[:, 0].values
-    Y = df.iloc[:, 1].values
-    plt.scatter(X,Y,c = kmeans.labels_, cmap = 'viridis')
-    plt.grid(True)
-    for center in kmeans.cluster_centers_:
-        center = center[:2]
-        plt.scatter(center[0],center[1],marker = '^',c = 'red')
-    plt.scatter(6.5, 4, marker='*')
-    plt.xlabel("petal length (cm)")
-    plt.ylabel("petal width (cm)")
-    return plt
+
+     # Define cluster names
+    cluster_names = {
+        0: "Politics",
+        1: "Business",
+        2: "Entertainment",
+        3: "Sports",
+        4: "Technology",
+    }
+
+    # Map cluster IDs to names
+    df["cluster_name"] = df["kmeans_cluster"].map(cluster_names)
+
+    # Calculate centroids for each cluster
+    centroids = df.groupby("kmeans_cluster")[["pca1", "pca2"]].mean()
+
+    # Plot with centroids labeled
+    sns.set(style="whitegrid")
+    plt.figure(figsize=(10, 8))
+
+    # Scatter plot
+    scatter = sns.scatterplot(
+        x="pca1",
+        y="pca2",
+        hue="cluster_name",
+        data=df,
+        palette="viridis",
+        s=100
+    )
+
+    # Annotate centroids with cluster names
+    for cluster_id, (x, y) in centroids.iterrows():
+        plt.text(
+            x, y,
+            cluster_names[cluster_id],
+            fontsize=10,
+            ha='center',  # Center-align text
+            bbox=dict(facecolor='white', edgecolor='black', boxstyle='round,pad=0.3')  # Add a box around the text
+        )
+        plt.scatter(
+            new_pca_point[0, 0],
+            new_pca_point[0, 1],
+            c="red",
+            s=200,
+            label="New Paragraph",
+            edgecolor="black"
+        )
+
+        plt.title("K-means Clustering with Cluster Names Annotated at Centroids")
+        plt.xlabel("PCA Dimension 1")
+        plt.ylabel("PCA Dimension 2")
+
+        plt.tight_layout()
+        return plt
 
 # @app.route('/predict', methods = ['POST'])
 # def predict():
